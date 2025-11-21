5e5aaee9f0497df275c09157db552f27a0509ad8
Awab Rao <mawabrao5@gmail.com>
2025-11-21 17:04:17 +0500

    changes
    dopied all from app.js into signin.js

---
+++ src/SIgnin.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import MainScreen from "./Pages/mainScreen.jsx";
+import SigninPage from "./Pages/SigninPage.jsx";
+import SignupPage from "./Pages/SignupPage.jsx";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainScreen />} />
+        
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
